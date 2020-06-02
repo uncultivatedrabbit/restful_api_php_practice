4e805208b8a1ada005c78a0b598ad09d92a2a797
Brandon Melendez <brandonmelendezdev@gmail.com>
2020-06-01 20:57:45 -0400

    first commit

---
+++ api/post/read.php
@@ -0,0 +1,53 @@
+<?php
+
+// Headers
+// accessable by anyone
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
+
+// Instantiate  and Connect DB 
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate blog post obj
+$post = new Post($db);
+
+// Blog post query
+$result = $post->read();
+
+// Get row count
+$num = $result->rowCount();
+
+// Check if any posts
+if ($num > 0){
+  // Post array
+  $posts_arr = array();
+  $posts_arr['data'] = array();
+
+  while($row = $result->fetch(PDO::FETCH_ASSOC)){
+    extract($row);
+    
+    $post_item = array(
+      'id' => $id,
+      'title' => $title,
+      'body' => html_entity_decode($body),
+      'author' => $author,
+      'category_id' => $category_id,
+      'category_name' => $category_name
+    );
+    
+    // Push post item to "data"
+    array_push($posts_arr["data"], $post_item);
+  }
+
+  // turn data to JSON and output
+  echo json_encode($posts_arr);
+} else {
+  // when no posts
+  echo json_encode(
+    array('message' => 'No Posts Found')
+  );
+}
---
+++ models/Post.php
@@ -0,0 +1,42 @@
+<?php
+
+class Post {
+  // DB properties
+  private $conn;
+  private $table = 'posts';
+  
+  //Post properties
+  public $id;
+  public $category_id;
+  public $category_name;
+  public $title;
+  public $body;
+  public $author;
+  public $created_at;
+
+  // constructor with DB
+  public function __construct($db)
+  {
+    $this->conn = $db;
+  }
+
+  // GET Posts
+  public function read(){
+    // DB query
+    $query = 'SELECT c.name 
+              as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+              FROM ' . $this->table . ' p
+              LEFT JOIN
+              categories c ON p.category_id = c.id
+              ORDER BY
+              p.created_at DESC';
+
+    // Prepare statement
+    $stmt = $this->conn->prepare($query);
+
+    // Execute query
+    $stmt->execute();
+    
+    return $stmt;
+  }
+}
